c819c543cdd5f6d4efdb2a0ed4d4e2fb7c1d4a87
Milos <56040933+m-stojanovic@users.noreply.github.com>
2020-09-05 15:21:29 +0200

    DevOps-4 Removing extras

--- dockerfile-grafana/bootstrap.sh
+++ dockerfile-grafana/bootstrap.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-echo '1' > /proc/sys/net/ipv6/conf/lo/disable_ipv6
-echo '1' > /proc/sys/net/ipv6/conf/lo/disable_ipv6
-echo '1' > /proc/sys/net/ipv6/conf/all/disable_ipv6
-echo '1' > /proc/sys/net/ipv6/conf/default/disable_ipv6
-
 service grafana-server start
 #/usr/sbin/grafana-server -config /etc/grafana/grafana.ini &
 
